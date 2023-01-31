2bdc069e9075e2c2b44366013486c5e3fbcd2110
GVRemington <104083156+GVRemington@users.noreply.github.com>
2023-01-31 10:52:45 -0800

    BookList Class Added

---
+++ RayleensGitTest/BookList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayleensGitTest
+{
+    internal class BookList
+    {
+        private string title;
+        private string description;
+        private string author;
+
+        public BookList(string Name, string AuthorLastName, string AuthorFirstName, string Description)
+        {
+            title = Name;
+            author = AuthorLastName +  ", " + AuthorFirstName;
+            description = Description;
+        }
+        
+        public string getBookTitle 
+        {
+            get { return title;}
+        }
+        public string getBookDescription
+        {
+            get { return description;}
+        }
+        public string getBookAuthor
+        {
+            get { return author;}
+        }
+    }
+}
--- RayleensGitTest/Program.cs
+++ RayleensGitTest/Program.cs
@@ -6,5 +6,11 @@ Calculator myCalc = new Calculator();
 Console.WriteLine(myCalc.Multiply(10, 5));
 Console.WriteLine(myCalc.Add(10, 5));
 Console.WriteLine(myCalc.Divide(10, 5));
-Console.WriteLine(myCalc.Subtract(10,5));
+Console.WriteLine(myCalc.Subtract(10, 5));
 Console.WriteLine(myCalc.added(10,5));
+
+BookList FirstBook = new BookList("Harry Potter", "Rowling", "J. K. ", "A wonderful book full of magic and wonder for children. ");
+
+
+Console.WriteLine(FirstBook.getBookTitle + ": " + FirstBook.getBookAuthor);
+Console.WriteLine(FirstBook.getBookDescription);
