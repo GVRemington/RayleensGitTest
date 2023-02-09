81e40fe1acccc0ae0ec96e21c6546399d5cee367
GVRemington <104083156+GVRemington@users.noreply.github.com>
2023-02-09 10:19:29 -0800

    Added a Second Booklist

--- RayleensGitTest/Program.cs
+++ RayleensGitTest/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine(myCalc.Subtract(10, 5));
 Console.WriteLine(myCalc.added(10,5));
 
 BookList FirstBook = new BookList("Harry Potter", "Rowling", "J. K. ", "A wonderful book full of magic and wonder for children. ");
-
+BookList SecondBook = new BookList("War and Peace", "Tolstoy ", "Leo ", "A wonderful book about war and peace ");
 
 Console.WriteLine(FirstBook.getBookTitle + ": " + FirstBook.getBookAuthor);
 Console.WriteLine(FirstBook.getBookDescription);
