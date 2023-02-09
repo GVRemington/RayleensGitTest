37044e2cd3ccb9cddf6faebb1bbec87e3ac05864
GVRemington <104083156+GVRemington@users.noreply.github.com>
2023-02-09 10:41:52 -0800

    Added Second Book and print out

--- RayleensGitTest/Program.cs
+++ RayleensGitTest/Program.cs
@@ -9,8 +9,10 @@ Console.WriteLine(myCalc.Divide(10, 5));
 Console.WriteLine(myCalc.Subtract(10, 5));
 Console.WriteLine(myCalc.added(10,5));
 
-Book FirstBook = new Book("Harry Potter", "Rowling", "J. K. ", "A wonderful book full of magic and wonder for children. ");
-
+Book FirstBook = new Book("Harry Potter ", "Rowling ", ",J. K. ", " A wonderful book full of magic and wonder for children. ");
+Book SecondBook = new Book("War and Peace ", "Tolstoy ", ",Leo ", " A wonderfull book full of war and peace ");
 
 Console.WriteLine(FirstBook.getBookTitle + ": " + FirstBook.getBookAuthor);
 Console.WriteLine(FirstBook.getBookDescription);
+Console.WriteLine(SecondBook.getBookTitle + ": " + SecondBook.getBookAuthor);
+Console.WriteLine(SecondBook.getBookDescription);
